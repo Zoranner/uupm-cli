16ae8a9c8a0215738549f77f1518f25a7ab77981
Zoranner <zoran_young@hotmail.com>
2024-07-13 16:25:40 +0800

    删除暂时无用的调试代码

--- src/actions/freeze-package.ts
+++ src/actions/freeze-package.ts
@@ -3,16 +3,14 @@ import { FreezePackageResolver } from './resolvers/index.js';
 
 const freezePackage = async () => {
   console.log(`Freezing project packages...`);
-  const configs = readConfigs();
-  console.log(JSON.stringify(configs));
-  const registryName = configs.registry.origin.default;
-  console.log(registryName);
-  const registrySource = configs.registry.origin.source;
-  console.log(registrySource);
-  console.log(registrySource[registryName]);
-  if (registrySource[registryName]) {
-
-  }
+  // const configs = readConfigs();
+  // console.log(JSON.stringify(configs));
+  // const registryName = configs.registry.origin.default;
+  // const registrySource = configs.registry.origin.source;
+  // console.log(registrySource);
+  // console.log(registrySource[registryName]);
+  // if (registrySource[registryName]) {
+  // }
   const resolver = new FreezePackageResolver();
   await resolver.recursionResolve();
   console.log(`Freeze finished!`);
